b6f359c53be5b0a1822a642fb8994f5bf8413f56
Burcu Bayraö <burju7@yahoo.com>
2023-06-12 17:13:28 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 import {
@@ -17,6 +18,9 @@ export default function App() {
 }
 
 function Example() {
+  const [clicked, setClicked] = useState(false);
+  const [clickedPost, setClickedPost] = useState();
+
   const { isLoading, error, data } = useQuery({
     queryKey: ["repoData"],
     queryFn: () =>
@@ -25,23 +29,51 @@ function Example() {
       ),
   });
 
-  console.log(data);
+  const handleClick = (e) => {
+    console.log("clicked");
+    console.log(e.target.innerText);
+    setClicked(true);
+    setClickedPost(e.target.innerText);
+  };
+
+  const handleBack = () => {
+    setClicked(false);
+  };
+
+  // console.log(data);
   if (isLoading) return "Loading...";
 
   if (error) return "An error has occurred: " + error.message;
 
+  console.log(clicked);
+
   return (
     <div>
-      {Array.isArray(data) &&
-        data.map((post, i) => {
-          return (
-            <div>
-              <h1 key={i}>{post.title}</h1>
-              <p key={i}>{post.body}</p>;<strong>👀 Id: {post.id}</strong>{" "}
-              <strong>✨User Id: {post.userId}</strong>{" "}
-            </div>
-          );
-        })}
+      {!clicked
+        ? Array.isArray(data) &&
+          data.map((post, i) => {
+            return (
+              <div key={i} style={{ border: "1px solid red" }}>
+                <h1 onClick={handleClick}>{post.title}</h1>
+                <p>{post.body}</p>
+                <strong>👀 Id: {post.id}</strong>{" "}
+                <strong>✨User Id: {post.userId}</strong>{" "}
+              </div>
+            );
+          })
+        : data
+            .filter((post) => post.title === clickedPost)
+            .map((element) => {
+              return (
+                <div key={element.id}>
+                  <h1>{element.title}</h1>
+                  <p>{element.body}</p>
+                  <strong>👀 Id: {element.id}</strong>{" "}
+                  <strong>✨User Id: {element.userId}</strong>{" "}
+                  <button onClick={handleBack}>Back to Posts</button>
+                </div>
+              );
+            })}
     </div>
   );
 }
